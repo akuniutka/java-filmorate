808ad10619a5cf9c8620fb14dc4dc3f40063daaf
Andrei Kuniutka <akuniutka@gmail.com>
2024-06-25 01:30:02 +0300

    Fix: Add request and response logging at endpoints

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,22 +17,32 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-        return userService.create(user);
+        log.info("Received POST at /users: {}", user);
+        final User savedUser = userService.create(user);
+        log.info("Responded to POST at /users: {}", savedUser);
+        return savedUser;
     }
 
     @GetMapping
     public Collection<User> findAll() {
-        return userService.findAll();
+        log.info("Received GET at /users");
+        final Collection<User> users = userService.findAll();
+        log.info("Responded to GET at /users: {}", users);
+        return users;
     }
 
     @PutMapping
     public User update(@Valid @RequestBody User user) {
-        return userService.update(user);
+        log.info("Received PUT at /users: {}", user);
+        final User savedUser = userService.update(user);
+        log.info("Responded to PUT at /users: {}", savedUser);
+        return savedUser;
     }
 }
