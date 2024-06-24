0a29eeeafee7fd50a8b137348101ca33b01808b7
Andrei Kuniutka <akuniutka@gmail.com>
2024-06-25 01:30:02 +0300

    Fix: Add request and response logging at endpoints

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,22 +17,32 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/films")
 @RequiredArgsConstructor
+@Slf4j
 public class FilmController {
 
     private final FilmService filmService;
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
-        return filmService.create(film);
+        log.info("Received POST at /films: {}", film);
+        final Film savedFilm = filmService.create(film);
+        log.info("Responded to POST at /films: {}", savedFilm);
+        return savedFilm;
     }
 
     @GetMapping
     public Collection<Film> finaAll() {
-        return filmService.findAll();
+        log.info("Received GET at /films");
+        final Collection<Film> films = filmService.findAll();
+        log.info("Responded to GET at /films: {}", films);
+        return films;
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
-        return filmService.update(film);
+        log.info("Received PUT at /films: {}", film);
+        final Film savedFilm = filmService.update(film);
+        log.info("Responded to PUT at /films: {}", savedFilm);
+        return savedFilm;
     }
 }
