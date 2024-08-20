23c115e74f2887d196b60c2b93218e6bb331af34
RuslanRinatovich <rusjlan4uk@mail.ru>
2024-08-20 10:12:31 +0300

    Создать модель Rewiew и RewiewLike

--- src/main/java/ru/yandex/practicum/filmorate/service/api/RewiewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/api/RewiewService.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.service.api;
 
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Rewiew;
 
 import java.util.Collection;
@@ -14,11 +13,15 @@ public interface RewiewService {
     Rewiew createRewiew(Rewiew rewiew);
 
     Optional<Rewiew> updateRewiew(Rewiew rewiew);
+
     void deleteRewiew(long id);
+
     void addLikeToRewiew(long id, long friendId);
+
     void addDislikeToRewiew(long id, long userId);
 
     void deleteLikeToRewiew(long id, long userId);
+
     void deleteDislikeToRewiew(long id, long userId);
 }
 
