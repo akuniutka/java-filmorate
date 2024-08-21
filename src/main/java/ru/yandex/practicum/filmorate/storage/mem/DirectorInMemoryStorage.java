83a623e805892295afd4e1758e72554538929d9e
Andrei Kuniutka <akuniutka@gmail.com>
2024-08-21 23:01:27 +0300

    Feat: Add service for model class Director

--- src/main/java/ru/yandex/practicum/filmorate/storage/mem/DirectorInMemoryStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mem/DirectorInMemoryStorage.java
@@ -1,8 +1,12 @@
 package ru.yandex.practicum.filmorate.storage.mem;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.storage.api.DirectorStorage;
 
+@Component
+@Slf4j
 public class DirectorInMemoryStorage extends BaseInMemoryStorage<Director> implements DirectorStorage {
 
     public DirectorInMemoryStorage() {
