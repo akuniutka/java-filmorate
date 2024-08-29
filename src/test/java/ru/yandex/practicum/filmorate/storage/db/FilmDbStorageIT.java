89ef12d8e14c030671c4df42a48d3f11e3b0e72d
Andrei Kuniutka <akuniutka@gmail.com>
2024-08-29 21:33:42 +0300

    Fix: Move Film repository to BeanPropertyRowMapper

--- src/test/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorageIT.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorageIT.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import ru.yandex.practicum.filmorate.storage.AbstractFilmStorageTest;
-import ru.yandex.practicum.filmorate.storage.db.mapper.FilmRowMapper;
 
 @JdbcTest
 class FilmDbStorageIT extends AbstractFilmStorageTest {
 
     @Autowired
     FilmDbStorageIT(final NamedParameterJdbcTemplate jdbc) {
-        this.filmStorage = new FilmDbStorage(jdbc, new FilmRowMapper());
+        this.filmStorage = new FilmDbStorage(jdbc);
     }
 }
